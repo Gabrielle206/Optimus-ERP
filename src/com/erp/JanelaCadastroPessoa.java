0e7d5dfd46425ef17ccc7ba48a3b675d3b9a3d17
minanarose <analuisalirasouza@gmail.com>
2025-10-23 20:27:10 -0300

    Update JanelaCadastroPessoa.java

--- src/com/erp/JanelaCadastroPessoa.java
+++ src/com/erp/JanelaCadastroPessoa.java
@@ -63,8 +63,12 @@ public class JanelaCadastroPessoa extends JDialog implements LanguageObserver {
         setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
         setLocationRelativeTo(getParent());
 
-        ImageIcon img = new ImageIcon("iconeerp/logo1.png");
-        setIconImage(img.getImage());
+        URL iconURL = getClass().getResource("/iconeerp/logo1.png");
+        if (iconURL != null) {
+            setIconImage(new ImageIcon(iconURL).getImage());
+        } else {
+            System.err.println("Erro: Não foi possível encontrar o ícone 'logo1.png'");
+        }
 
         JPanel painelPrincipal = new JPanel(new BorderLayout(10, 10));
         painelPrincipal.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
